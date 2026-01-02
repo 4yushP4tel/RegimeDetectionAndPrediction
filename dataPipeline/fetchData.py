2de0fd158ecc26084eb9f55be84c96f63e25c4ad
Ayush <payush548@gmail.com>
2026-01-02 14:27:23 -0500

    able to get data for index and for holdings

--- dataPipeline/fetchData.py
+++ dataPipeline/fetchData.py
@@ -1,7 +1,5 @@
 from datetime import datetime
-import re
-from turtle import st
-from altair import Time
+from typing import Optional
 import pandas as pd
 import numpy as np
 import websocket
@@ -21,6 +19,17 @@ ALPACA_API_KEY = os.getenv("ALPACA_API_KEY")
 ALPACA_API_SECRET_KEY = os.getenv("ALPACA_API_SECRET_KEY")
 
 class DataPipeLine:
+    """
+    Using XLK as the tech sector etf
+    """
+
+    # these holdings could be used to find the correlation structure
+    # which could be used as an endogenous feature in HMM
+    TOP_HOLDINGS_XLK = ["NVDA", "AAPL", "MSFT", "AVGO", "PLTR", "AMD", "ORCL",
+                        "MU", "CSCO", "IBM"]
+    
+    COL_ORDER = ["date","close","open","high","low","volume"]
+    TRAINING_COLS = []
     def __init__(self):
         self.client = None
         self.stream = None
@@ -33,31 +42,64 @@ class DataPipeLine:
         except Exception as e:
             print(f"Error connecting to Alpace: {e}")
 
-    def fetch_historical_data(self):
+    def fetch_historical_data(self, symbol: str, start_date: Optional[datetime]=None, end_date: Optional[datetime]=None) -> pd.DataFrame:
         try:
-            response = self.client.get_stock_bars(StockBarsRequest(symbol_or_symbols="XLK", 
+            response = self.client.get_stock_bars(StockBarsRequest(symbol_or_symbols=symbol, 
                                               timeframe=TimeFrame(1, TimeFrameUnit.Day),
-                                              start=datetime(2020, 1, 1),
-                                              end=datetime(2025, 12, 31)))
+                                              start=start_date,
+                                              end=end_date))
             df = response.df
+            print(f"Response df for {symbol}: \n {df}")
             df["date"] = df.index.get_level_values(1).strftime("%Y-%m-%d")
             df.reset_index(drop=True, inplace=True)
-            
+            df = df[self.COL_ORDER]
+            return df
         except Exception as e:
             print(f"Error fetching historical data: {e}")
+
+    def fetch_full_historical_data(self, symbol: str) -> pd.DataFrame:
+        return self.fetch_historical_data(symbol, datetime(1900,1,1))
 
     def fetch_live_data(self):
         try:
             pass
         except Exception as e:
             print(f"Error fetching live data: {e}")
+        
+    def fetch_and_transform_holdings_data(self) -> pd.DataFrame:
+        df_mapping = {}
+        for holding in self.TOP_HOLDINGS_XLK:
+            temp_df = self.fetch_full_historical_data(holding)
+            temp_df = temp_df.set_index("date")
+            DataPipeLine._get_log_returns(temp_df)
+            print(f"Here is the data for : {holding}\n {temp_df}")
+            print()
+            df_mapping[holding] = temp_df
+        
+        combined_log_returns_df = pd.concat(df_mapping, axis=1, ignore_index=True)
+        return combined_log_returns_df
 
-    def display_data_in_terminal(self):
+    def compute_correlation_matrix_of_holdings(self, log_returns_df: pd.DataFrame):
         pass
 
-    def get_training_data(self):
-        pass 
+    @staticmethod
+    def _get_log_returns(df: pd.DataFrame):
+        #using the close prices for this
+        df["log_returns"] = np.log(df["close"]/df["close"].shift(1))
+        df.dropna(inplace=True)
 
+    @staticmethod
+    def _get_intraday_price_range(df: pd.DataFrame):
+        df["price_range"] = abs(df["high"] - df["low"])
+
+    @staticmethod
+    def _get_rolling_stats(df: pd.DataFrame):
+        pass
+
+    @staticmethod
+    def get_training_data(df):
+        DataPipeLine._get_intraday_price_range(df)
+        DataPipeLine._get_log_returns(df)
 
     def get_implied_vol(self):
         """
@@ -65,23 +107,33 @@ class DataPipeLine:
         """
         pass
 
-    def get_vix(self):
+    def get_VIX(self):
         pass
     
     @staticmethod
     def save_data_to_csv(df: pd.DataFrame, file_name:str):
-        file_path = os.path.join(os.path.dirname(__file__), '..', 'data', file_name)
+        file_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'data', file_name))
         df.to_csv(file_path, index=False)
 
     @staticmethod
     def save_data_to_existing_csv(df: pd.DataFrame, file_name:str):
-        file_path = os.path.join(os.path.dirname(__file__), '..', 'data', file_name)
-        old_df = pd.read_csv(file_name)
+        file_abs_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'data', file_name))
+        print(f"Saving data to existing csv at {file_abs_path}")
+        old_df = pd.read_csv(file_abs_path)
         df = pd.concat([old_df, df], axis= 0, ignore_index=True)
+        df.drop_duplicates(subset=["date"], inplace=True)
         df.sort_values(by="date", inplace=True)
-        df.to_csv(file_path, index=False)
+        df.to_csv(file_abs_path, index=False)
 
+    @staticmethod
+    def change_csv_date_format(file_name:str):
+        # used for the data which I got online
+        file = os.path.join(os.path.dirname(__file__), '..', 'data', file_name)
+        df = pd.read_csv(file)
+        df["date"] = df["date"].str.split("T").str[0]
+        df.reset_index(drop=True, inplace=True)
+        df.to_csv(file, index=False)
 
 if __name__ == "__main__":
     dp = DataPipeLine()
-    dp.fetch_historical_data()
+    dp.fetch_and_transform_holdings_data()
